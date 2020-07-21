0bead450ea851fc609f7a203397ef7bda4a47ae1
Andre Freeman <asf@mail.usf.edu>
2020-07-20 22:48:17 -0400

    Endpoints working with auth

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,63 +1,61 @@
-const router = require('express').Router();
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const restrict = require("./authenticate-middleware")
-const Users = require("./model")
-
-
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const restrict = require("./authenticate-middleware");
+const Users = require("./model");
 
 router.post("/register", async (req, res, next) => {
-	try {
-		const { username, password } = req.body
-		const user = await Users.findBy({ username }).first()
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
 
-		if (user) {
-			return res.status(409).json({
-				message: "Username is already taken",
-			})
-		}
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken"
+      });
+    }
 
-		const newUser = await Users.add({
-			username,
+    const newUser = await Users.add({
+      username,
 
-			password: await bcrypt.hash(password, 5),
-		})
+      password: await bcrypt.hash(password, 5)
+    });
 
-		res.status(201).json(newUser)
-	} catch(err) {
-		next(err)
-	}
-})
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/login", async (req, res, next) => {
-	try {
-		const { username, password } = req.body
-		const user = await Users.findBy({ username }).first()
-		
-		if (!user) {
-			return res.status(401).json({
-				message: "Invalid Credentials",
-			})
-		}
-		const passwordValid = await bcrypt.compare(password, user.password)
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
 
-		if (!passwordValid) {
-			return res.status(401).json({
-				message: "Invalid Credentials",
-			})
-		}
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials"
+      });
+    }
+    const passwordValid = await bcrypt.compare(password, user.password);
 
-		const payload = {
-			userId: user.id,
-			username: user.username,
-		}
-		res.cookie("token", jwt.sign(payload, process.env.JWT_SECRET))
-		res.json({
-			message: `Welcome ${user.username}!`,
-		})
-	} catch(err) {
-		next(err)
-	}
-})
+    if (!passwordValid) {
+      return res.status(401).json({
+        you: "shall not pass!"
+      });
+    }
+
+    const payload = {
+      userId: user.id,
+      username: user.username
+    };
+    res.cookie("token", jwt.sign(payload, process.env.JWT_SECRET));
+    res.json({
+      message: `Welcome ${user.username}!`
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,35 +1,33 @@
-const jwt = require("jsonwebtoken")
-
+const jwt = require("jsonwebtoken");
 
 function restrict(role) {
-	return async (req, res, next) => {
-		const authError = {
-			you: "shall not pass!",
-		}
+  return async (req, res, next) => {
+    const authError = {
+      you: "shall not pass!"
+    };
 
-		try {
-			const token = req.cookies.token
+    try {
+      const token = req.cookies.token;
 
-			if (!token) {
-				return res.status(401).json(authError)
-			}
+      if (!token) {
+        return res.status(401).json(authError);
+      }
 
-			jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-				if (err) {
-					return res.status(401).json(authError)
-			}
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json(authError);
+        }
 
-			if (role && decoded.userRole !== role) {
-				return res.status(401).json(authError)
-			}
+        if (role && decoded.userRole !== role) {
+          return res.status(401).json(authError);
+        }
 
-			next()
-			})
-		} catch(err) {
-			next(err)
-		}
-	}
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 }
 
-module.exports = restrict
-
+module.exports = restrict;
