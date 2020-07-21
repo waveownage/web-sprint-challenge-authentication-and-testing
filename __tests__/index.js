f73af6cfe7a86219130e1c30d53ff0cb0914777b
Andre Freeman <asf@mail.usf.edu>
2020-07-21 00:17:03 -0400

    tidy up

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,36 +1,29 @@
-const supertest = require("supertest")
-const server = require("../api/server")
-const db = require("../database/dbConfig")
-
-
-
+const supertest = require("supertest");
+const server = require("../api/server");
+const db = require("../database/dbConfig");
 
 describe("Tests", () => {
+  it("POST /register", async () => {
+    const res = await supertest(server)
+      .post("/api/auth/register")
+      .send({ username: "andre", password: "1234" });
+    expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.body.username).toBe("andre");
+    expect(res.statusCode).toBe(201);
+  });
 
-    it("POST /register", async () => {
-        const res = await supertest(server)
-        .post("/api/auth/register") 
-        .send({ username: "andre", password: "1234"})
-        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-        expect(res.body.username).toBe("andre")
-        expect(res.statusCode).toBe(201)
+  it("POST /login", async () => {
+    const res = await supertest(server)
+      .post("/api/auth/login")
+      .send({ username: "andre", password: "1234" });
+    expect(res.body.message).toBe("Welcome andre!");
+    expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+  });
 
-
-    })
-
-    it("POST /login", async () => {
-		const res = await supertest(server)
-			.post("/api/auth/login")
-            .send({ username: "andre", password: "1234" })
-        expect(res.body.message).toBe("Welcome andre!")
-        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-        expect(res.statusCode).toBe(200)
-    })
-
-    it("GET /jokes", async () => {
-		const res = await supertest(server)
-            .get("/api/jokes")
-        expect(res.statusCode).toBe(200)
-        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-    })
-})
+  it("GET /jokes", async () => {
+    const res = await supertest(server).get("/api/jokes");
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+  });
+});
